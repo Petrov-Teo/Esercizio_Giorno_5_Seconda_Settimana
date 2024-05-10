0506151ccb2ba7829b6752d25a8b5acfa3e81bb5
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-05-10 16:39:27 +0200

    esercizio 4

--- W2-D5/JS Exercises/D5.js
+++ W2-D5/JS Exercises/D5.js
@@ -20,14 +20,21 @@ for (let index = 0; index < pets.length; index++) {
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
+pets.sort();
+console.log(pets);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
-
+pets.reverse();
+console.log(pets);
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+pets.shift();
+console.log(pets);
+pets.push("redfish");
+console.log(pets);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
